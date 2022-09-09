3f1f2007dd9fdc0ee2bced77732ead427390ac9f
Andrew Doherty <andrewdoherty1996@hotmail.co.uk>
2022-09-09 20:19:49 +0100

    created file structure and tested js

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.scss';
+
+function App() {
+
+console.log("i work");
+
+  return (
+    <div className="app">
+      <header>
+        <h1>PUNK API PROJECT</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
